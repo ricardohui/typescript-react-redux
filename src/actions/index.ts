2cd4ab6566db22fddddb027b984fa22763366779
Ricardo Hui <eehaha@gmail.com>
2019-08-27 22:10:29 +0800

    connect component to redux

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 import { Dispatch } from "redux";
 import { ActionTypes } from "./types";
-interface ITodo {
+export interface ITodo {
   id: number;
   title: string;
   completed: boolean;
 }
-interface FetchTodosAction {
+export interface FetchTodosAction {
   type: ActionTypes.fetchTodos;
   payload: ITodo[];
 }
-const url = "https://jsonplaceholder.typicode/todos";
+const url = "https://jsonplaceholder.typicode.com/todos";
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<ITodo[]>(url);
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,39 @@
 import React from "react";
-export class App extends React.Component {
+import { connect } from "react-redux";
+import { ITodo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
+
+interface AppProps {
+  todos: ITodo[];
+  fetchTodos(): any;
+}
+class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: ITodo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
   render() {
-    return <div>Hi</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
+const mapStateToProps = (state: StoreState) => {
+  return { todos: state.todos };
+};
+// can be further simplified to this
+// const mapStateToProps = ({todos}: StoreState) => {
+//   return { todos };
+// };
+
+export const App = connect(
+  mapStateToProps,
+  { fetchTodos }
+)(_App);
